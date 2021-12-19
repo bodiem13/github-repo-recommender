cb054308a2e12b619b2f36477a8031ff5a1c18be
bodiem13 <bodiem286@gmail.com>
2021-12-18 21:10:34 -0500

    Did stuff

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,15 +3,24 @@ import {SearchBarStyling} from './SearchBarElements'
 
 //props of repoUrl and getRepoDetails are passed to the component
 const SearchBar = ({input:repoUrl, onChange:getRepoDetails}) => {
+    function collectSearch(e){
+        console.log("The collectSearch function is being ran")
+    }
+
     return(
         <div>
-            <input 
+            <form onSubmit={collectSearch}>
+                <input 
                 style={SearchBarStyling}
                 key="foo"
                 value={repoUrl}
                 placeholder={"Enter GitHub Repository Link"}
                 onChange={(e) => getRepoDetails(e.target.value)}
-            />
+                />
+                <button type="submit">
+                    Search
+                </button>
+            </form>
         </div>
     );
 };
