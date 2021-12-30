70f87b5d4a917b3f284ff4a43d8ff20f208e89a6
bodiem13 <bodiem286@gmail.com>
2021-12-30 14:26:49 -0500

    Added craco for build

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
