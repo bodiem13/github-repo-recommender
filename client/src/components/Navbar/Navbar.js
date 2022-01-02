34d1d0b25da94c3fb97d1aa9f598e45b271f5719
bodiem13 <bodiem286@gmail.com>
2022-01-01 20:28:59 -0500

    Updated

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { Bars } from './NavbarElements'
 
 const Navbar = () => {
     //FaGithub
@@ -9,13 +10,19 @@ const Navbar = () => {
                 <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0" href={"/"}>
                     <FaGithub className="text-5xl text-white"/>
                 </a>
-                <div className="hidden text-white md:block md:absolute md:top-0 md:right-0 md:text-7xl md:cursor-pointer md:translate-y-3/4">
+                {/*<div className="hidden text-white md:block md:absolute md:top-0 md:right-0 md:text-7xl md:cursor-pointer md:translate-y-3/4">*/}
+                {/*<Bars />*/}
+                <div className="text-white items-center">
                     <div className="flex items-center mr-5 text-2xl">
                         <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white" href={"/about"}>
                             About
                         </a>
+                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white" href={"/results"}>
+                            Results
+                        </a>
                     </div>
                 </div>
+                {/*</div>*/}
             </div>
         </div>
     );
