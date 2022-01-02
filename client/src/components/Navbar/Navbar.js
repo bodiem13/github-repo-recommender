dd18947e72f04f82227fecbec4ed4e6f72668f13
bodiem13 <bodiem286@gmail.com>
2022-01-01 21:25:43 -0500

    loop through hyperlink tags

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,14 @@ import { FaGithub } from 'react-icons/fa';
 
 //Navbar for navigation to additional pages and back to home screen
 const Navbar = () => {
+    //list of navbar tabs to loop through when creating hyperlink tags
+    const navbarTabs = ['Results', 'About']
+    const navbarTabsTags = []
+
+    for (let tab of navbarTabs) {
+        const tabLink = "/"+tab;
+        navbarTabsTags.push(<a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white hover:text-gray-400" href={tabLink}>{tab}</a>)
+    }
     return(
         <div>
             <div className="bg-gray-github h-20 flex justify-between z-10 items-center">
@@ -11,12 +19,7 @@ const Navbar = () => {
                 </a>
                 <div className="text-white items-center">
                     <div className="flex items-center mr-5 text-2xl">
-                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white hover:text-gray-400" href={"/about"}>
-                            Results
-                        </a>
-                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white hover:text-gray-400" href={"/results"}>
-                            About
-                        </a>
+                    {navbarTabsTags}
                     </div>
                 </div>
             </div>
