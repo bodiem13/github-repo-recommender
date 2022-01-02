da064807817c2ed3779620a89696a465bc1566d9
bodiem13 <bodiem286@gmail.com>
2022-01-01 20:58:11 -0500

    Fixed style format

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
-import { Bars } from './NavbarElements'
 
+//Navbar for navigation to additional pages and back to home screen
 const Navbar = () => {
-    //FaGithub
     return(
         <div>
-            <div className="bg-gray-github h-20 flex justify-between z-10">
+            <div className="bg-gray-github h-20 flex justify-between z-10 items-center">
                 <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0" href={"/"}>
                     <FaGithub className="text-5xl text-white"/>
                 </a>
-                {/*<div className="hidden text-white md:block md:absolute md:top-0 md:right-0 md:text-7xl md:cursor-pointer md:translate-y-3/4">*/}
-                {/*<Bars />*/}
                 <div className="text-white items-center">
                     <div className="flex items-center mr-5 text-2xl">
-                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white" href={"/about"}>
-                            About
-                        </a>
-                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white" href={"/results"}>
+                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white hover:text-gray-400" href={"/about"}>
                             Results
+                        </a>
+                        <a className="flex items-center no-underline h-full cursor-pointer px-4 py-0 text-white hover:text-gray-400" href={"/results"}>
+                            About
                         </a>
                     </div>
                 </div>
-                {/*</div>*/}
             </div>
         </div>
     );
