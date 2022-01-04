5b7aba6f586ae1324fa1786600289e8355e23f23
bodiem13 <bodiem286@gmail.com>
2022-01-03 21:25:03 -0500

    Migrated api

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls.converters import DEFAULT_CONVERTERS
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
