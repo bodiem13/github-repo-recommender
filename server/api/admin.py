d587eac25fa3e7dcd312a341be6a985379d3017f
bodiem13 <bodiem286@gmail.com>
2022-01-07 19:51:05 -0500

    Configured admin screen

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+#checking django admin panel shows Articles 
+#admin.site.register(Article)
+
+# adds filters to admin panel
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
+
--- server/api/models.py
+++ server/api/models.py
@@ -3,6 +3,12 @@ from django.urls.converters import DEFAULT_CONVERTERS
 
 # Create your models here.
 
+#create an Article table in database
 class Article(models.Model):
+    #create two fields in model
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+    #in admin console, objects will now show title
+    def __str__(self):
+        return self.title
