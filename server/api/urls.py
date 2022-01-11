31e703e70fd87ded3eb8e87f5ccf00d341dd6691
bodiem13 <bodiem286@gmail.com>
2022-01-10 20:39:58 -0500

    Updated with viewsets and routers

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,11 +1,15 @@
 
-from django.urls import path
-from .views import ArticleList, ArticleDetails
+from django.urls import path, include
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 #article_list, article_details
 
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 urlpatterns= [
-    path('articles/', ArticleList.as_view()),
-    path('articles/<int:id>/', ArticleDetails.as_view())
+    path('', include(router.urls)),
+    #path('articles/', ArticleList.as_view()),
+    #path('articles/<int:id>/', ArticleDetails.as_view())
     ]
 
 # urlpatterns = [
--- server/api/views.py
+++ server/api/views.py
@@ -10,76 +10,73 @@ from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-#create our class that extends from the APIView decorator in rest_framework
-class ArticleList(generics.GenericAPIView, mixins.ListModelMixin,
-mixins.CreateModelMixin):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-    def get(self, request):
-        return self.list(request)
-
-    def post(self, request):
-        return self.create(request)
-
-    # def get(self, request):
-    #     if request.method == 'GET':
-    #         #get all articles from database and store in articles variable
-    #         articles = Article.objects.all()
-    #         #add many=True if articles is a query set
-    #         serializer = ArticleSerializer(articles, many=True)
-    #         return Response(serializer.data)
+#specify crud operations when using ViewSet
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
     
-    # def post(self, request):
-    #     serializer = ArticleSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-class ArticleDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-    #create queryset and create serializer class
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    lookup_field = 'id'
-    
-    def get(self, request, id):
-        return self.retrieve(request, id=id)
-    
-    def put(self, request, id):
-        return self.update(request, id=id)
 
-    def delete(self, request, id):
-        return self.destroy(request, id)
+        
 
 
-    # def get_object(self, id):
-    #     try:
-    #         return Article.objects.get(id=id)
-    #     except Article.DoesNotExist:
-    #         return Response(status=status.HTTP_404_NOT_FOUND)
+# #create our class that extends from the APIView decorator in rest_framework
+# class ArticleList(generics.GenericAPIView, mixins.ListModelMixin,
+# mixins.CreateModelMixin):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+#     def get(self, request):
+#         return self.list(request)
+
+#     def post(self, request):
+#         return self.create(request)
+
+# class ArticleDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+#     #create queryset and create serializer class
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     lookup_field = 'id'
     
-    # def get(self, request, id):
-    #     #get all articles from database and store in articles variable
-    #     articles = self.get_object(id)
-    #     #add many=True if articles is a query set
-    #     serializer = ArticleSerializer(articles)
-    #     return Response(serializer.data)
+#     def get(self, request, id):
+#         return self.retrieve(request, id=id)
     
-    # def put(self, request, id):
-    #     article = self.get_object(id)
-    #     serializer = ArticleSerializer(article, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    # def delete(self, request, id):
-    #     article = self.get_object(id)
-    #     article.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+#     def put(self, request, id):
+#         return self.update(request, id=id)
+
+#     def delete(self, request, id):
+#         return self.destroy(request, id)
 
 
 #get all articles
