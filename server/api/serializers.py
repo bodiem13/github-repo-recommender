9d1049c3ae0209d7036047870b50137d69b04bcb
bodiem13 <bodiem286@gmail.com>
2022-01-17 15:36:16 -0500

    Added user view set

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Article
+from django.contrib.auth.models import User
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         #specify the fields to be serialized
         fields = ['id', 'title', 'description']
-        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']       
     
--- server/api/urls.py
+++ server/api/urls.py
@@ -1,13 +1,15 @@
 
 from django.urls import path, include
-from .views import ArticleViewSet
+from .views import ArticleViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
+
 #article_list, article_details
 
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='articles')
+router.register('users', UserViewSet)
 urlpatterns= [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     #path('articles/', ArticleList.as_view()),
     #path('articles/<int:id>/', ArticleDetails.as_view())
     ]
