d1e9c00eba3a6977f80558e82bec6a407c5fdc99
bodiem13 <bodiem286@gmail.com>
2022-02-21 14:24:58 -0500

    Add data extraction

--- server/models/src/APIServices.py
+++ server/models/src/APIServices.py
@@ -1,24 +1,47 @@
 import requests
+import json
 
 class githubAPIServices:
 
     def __init__(self):
         self.api_url = 'https://api.github.com/'
-        self.api_token = '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'
+        #self.api_token = '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'
+        self.api_token = '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'
         self.headers = {'Authorization': 'token %s' % self.api_token}
 
     def getRepositoriesByStars(self, numRepos):
         #api url to grab public user repositories
         #api_url = f"https://api.github.com/repos/bodiem13/github-repo-recommender"
         repos = requests.get(self.api_url+'repositories?sort=stars&order=desc', headers=self.headers).json()
-        print(repos)
         for repo in repos:
             repoName = repo['name']
             owner = repo['owner']['login'] 
-            repoInfo = requests.get(self.api_url + 'repos/'+owner+'/'+repoName)
-            stars = repoInfo.json()['stargazers_count']
-            print(repoName, stars)
-        print(repos)
+            repoInfo = requests.get(self.api_url + 'repos/'+owner+'/'+repoName).json()
+            with open('server/models/src/data.json', 'w') as f:
+                json.dump(repoInfo, f)
+            size = repoInfo['size']
+            watchers_count = repoInfo['watchers_count']
+            has_issues = repoInfo['has_issues']
+            has_wiki = repoInfo['has_wiki']
+            has_pages = repoInfo['has_pages']
+            has_projects = repoInfo['has_projects']
+            forks_count = repoInfo['forks_count']
+            open_issues_count = repoInfo['open_issues_count']
+            subscribers_count = repoInfo['subscribers_count']
+            is_template = repoInfo['is_template']
+            num_topics = len(repoInfo['topics'])
+            repoInfo = requests.get(self.api_url + 'repos/'+owner+'/'+repoName+'/branches').json()
+            with open('server/models/src/data.json', 'w') as f:
+                json.dump(repoInfo, f)
+            num_branches = len(repoInfo)
+            #check commits
+            repoInfo = requests.get(self.api_url + 'repos/'+owner+'/'+repoName+'/commits').json()
+            with open('server/models/src/data.json', 'w') as f:
+                json.dump(repoInfo, f)
+            
+            #stars = repoInfo.json()['stargazers_count']
+            print("Done")
+            
 
 githubAPIServices = githubAPIServices()
 githubAPIServices.getRepositoriesByStars(5)
