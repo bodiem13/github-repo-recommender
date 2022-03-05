0a696260b296e8b8df6eb900d7bd43e1b4240534
bodiem13 <bodiem286@gmail.com>
2022-03-05 15:35:58 -0500

    Added results page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Contact from './pages/contact'
+import Results from './pages/results'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path={"/"} element={<Home/>} />
+        <Route path={"/results"} element={<Results/>} />
         <Route path={"/contact"} element={<Contact/>} />
       </Routes>
     </Router>
---
+++ client/src/pages/results.js
@@ -0,0 +1,80 @@
+import React, {useState, useEffect} from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import SearchBar from '../components/SearchBar/SearchBar'
+
+const Results = () => {
+    //creates my state and use state
+    const [articles, setArticles] = useState([])
+
+
+    const [editArticle, setEditArticle] = useState(null)
+
+    //use useEffect to fetch the data
+    useEffect(() => {
+        //routes to api
+        //can pass data
+        fetch('http://127.0.0.1:8000/api/articles/', {
+            'method': 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token a6d0b482c2f311bea80b5375eeee5413ce47bb30'
+            }
+        })
+        //get promise from api call
+        .then(resp => resp.json())
+        //set response to articles
+        .then(resp => setArticles(resp))
+        //error handling
+        .catch(error => console.log(error))
+    }, [])
+
+    const editBtn = (article) => {
+        setEditArticle(article)
+    }
+    
+    const updatedInformation = (article) => {
+        const new_article = articles.map(myarticle => {
+            if (myarticle.id === article.id){
+                return article;
+            }
+            else {
+                return myarticle;
+            }
+        })
+        setArticles(new_article)
+    }
+
+    const articleForm = (repoUrl) => {
+        //setEditArticle({title: '', description: ''})
+        console.log("Printing repoUrl from home page")
+        console.log(repoUrl)
+    }
+
+    const insertedInformation = (article) => {
+        const new_articles = [...articles, article]
+        setArticles(new_articles)
+    }
+
+    const deleteBtn = (article) => {
+        const new_articles = articles.filter(myarticle => {
+            if (myarticle.id === article.id){
+                return false
+            }
+            else {
+                return true
+            }
+        })
+
+        setArticles(new_articles)
+    }
+
+
+    return (
+        <div className="justify-center align-middle h-32">
+        <Navbar />
+        <SearchBar placeholder = "Enter a GitHub Repository Url..." articleForm = {articleForm} insertedInformation = {insertedInformation}/>
+        </div>
+    )
+}
+
+export default Results
